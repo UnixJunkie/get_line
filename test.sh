468db41c647100fc21f824a2c9b3ce826e6b45d1
Francois BERENGER <ligand@free.fr>
2017-01-25 21:12:54 -0600

    more tests

--- test.sh
+++ test.sh
@@ -4,10 +4,17 @@ tmp=`mktemp`
 
 seq 1 10 > $tmp
 
+set -x
+
 ./get_line 1 $tmp
 ./get_line 2 $tmp
 ./get_line 3 $tmp
+./get_line 2-5 $tmp
 ./get_line 0 $tmp
 ./get_line 11 $tmp
+./get_line 10-12 $tmp
+./get_line 12-15 $tmp
+
+set +x
 
 rm -f $tmp
